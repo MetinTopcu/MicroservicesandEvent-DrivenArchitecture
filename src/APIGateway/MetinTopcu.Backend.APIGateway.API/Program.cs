80f91c6794fd07e6cda3d577edb9932c1cf2d3b7
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2025-04-06 15:18:02 +0300

    Mock API Gateway created

--- src/APIGateway/MetinTopcu.Backend.APIGateway.API/Program.cs
+++ src/APIGateway/MetinTopcu.Backend.APIGateway.API/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("Configuration/ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"Configuration/ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
 // Add services to the container.
+
+builder.Services.AddOcelot();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +17,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+await app.UseOcelot();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
