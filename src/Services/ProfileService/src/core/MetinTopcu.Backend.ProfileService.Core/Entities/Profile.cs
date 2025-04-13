e316f182619d61cca3db3e5e1561bab0d4578347
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2025-04-13 23:54:05 +0300

    Repositories created

--- src/Services/ProfileService/src/core/MetinTopcu.Backend.ProfileService.Core/Entities/Profile.cs
+++ src/Services/ProfileService/src/core/MetinTopcu.Backend.ProfileService.Core/Entities/Profile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetinTopcu.Backend.src.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace MetinTopcu.Backend.ProfileService.Core.Entities
 {
-    public class Profile
+    public class Profile : IEntity<Guid>
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
         public string GitHub { get; set; }
---
+++ src/Services/ProfileService/src/data/MetinTopcu.Backend.ProfileService.Data/Repositories/ProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using MetinTopcu.Backend.ProfileService.Core.Entities;
+using MetinTopcu.Backend.ProfileService.Data.Abstractions;
+using MetinTopcu.Backend.src.Domain.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetinTopcu.Backend.ProfileService.Data.Repositories
+{
+    public class ProfileRepository<TDbContext> : GenericRepository<Profile, TDbContext, Guid>, IProfileRepository where TDbContext : DbContext
+    {
+        public ProfileRepository(TDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
